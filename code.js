c98257191a33f0a53a0aa262a0b26c831ca95a77
DumbAugust <carlaugust.sponberg@gmail.com>
2024-04-05 21:02:59 +0200

    i need to make the camera follow the character somehow.

--- code.js
+++ code.js
@@ -1,34 +1,35 @@
 const map = document.getElementById('map')
 const player = document.getElementById('player')
 
-// character positions
-
+let speed = 5 // player speed
 let x = 0
 let y = 0
 
-let speed = 1 // player speed
+function move(key) {
+
+    switch(key) {
+        case 'W':
+            y -= speed;
+            break;
+        case 'A':
+            x -= speed;
+            break;
+        case 'D':
+            x += speed;
+            break;
+        case 'S':
+            y  += speed;
+    } 
+    player.style.top = y + "px"
+    player.style.left = x + "px"
+}
 
 document.addEventListener('keydown', function(event) {
-    var key = event.key.toUpperCase()
+
+    let key = event.key.toUpperCase()
     if (['W', 'A', 'S', 'D'].includes(key)) {
-
-        switch(key) {
-            case 'W':
-                y += speed;
-                break;
-            case 'A':
-                x -= speed
-                break;
-            case 'D':
-                x += speed
-                break;
-            case 'S':
-                y -= speed
-        }
-
-        console.log('click')
-        
+        move(key);
     }
 
-    player.style.top += y
+        
 });
