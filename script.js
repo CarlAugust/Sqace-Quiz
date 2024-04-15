6636cea65d524ce7244256a86ec22531a4d7dda9
DumbAugust <carlaugust.sponberg@gmail.com>
2024-04-15 13:00:20 +0200

    Canvas resets every loop

--- script.js
+++ script.js
@@ -5,7 +5,39 @@ canvas.height = window.innerHeight
 c = canvas.getContext("2d")
 c.translate(canvas.width/2,canvas.height/2);
 
-c.beginPath();
+let a = 0;
+let angle = 0
 
-c.arc(0, 0, 100, 0, Math.PI * 2, false);
-c.stroke();
+function globalNum(){
+    a += 0.001
+    if (a > 6.28) {
+        a = 0
+    }
+}
+
+function orbit() {
+
+    angle = Math.round((a*180) / 3.14)
+
+}
+
+function animate(){
+    requestAnimationFrame(animate);
+    globalNum();
+    orbit();
+
+    let x = Math.round(Math.cos(angle) * 200)
+    let y = Math.round(Math.sin(angle) * 200)
+
+    c.beginPath();
+    c.clearRect(-canvas.width,-canvas.height, canvas.width*2, canvas.height*2)
+    c.arc(0, 0, 50, 0, Math.PI*2, false)
+    c.stroke();
+    c.closePath()
+    c.beginPath()
+    c.arc(x, y, 10, 0, Math.PI*2, false)
+    c.stroke()
+
+}
+
+animate();
