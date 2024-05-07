b6e9498589a016f11caffa423728de89f435666c
DumbAugust <carlaugust.sponberg@gmail.com>
2024-05-07 22:32:23 +0200

    Added som text for when you win

--- scripts/planets.js
+++ scripts/planets.js
@@ -1,5 +1,3 @@
-
-
 const canvas = document.querySelector('canvas')
 let root = document.querySelector('.root')
 
@@ -14,33 +12,37 @@ c = canvas.getContext("2d")
 let a = 0;
 
 // Variable for how fast the game runs
-let originalSpeed = 0.00005
+let originalSpeed = -0.00005
 let runSpeed = originalSpeed
 
 
-// A global a variable which increments up to 20*PI
+// A global a variable which increments up to 8*PI
+// This variable is used by all planets to calculate angles
 function globalNum(){
     a += runSpeed
-
-    if (a > Math.PI * 8) {
-        a -= Math.PI * 8
+    if (a <  -Math.PI * 8) {
+        a += Math.PI * 8
     }
 
 }
 
-
-
+// Array that stores all planet objects
 let planets = [];
 
+// planet class, planet refers to all celestial objects including the sun
 class planet {
 
-    constructor(radius, speed, xPath, yPath, name) {
+    /* A constructor that makes all the values in the planet object
+    it takes a radius for the size, a speed for how fast it rotaes around the center
+    Takes a xPath and yPath, both contributes to making oval shaped
+    planet orbit paths */
+    constructor(radius, speed, xPath, yPath, obj) {
 
         this.radius = radius
         this.speed = speed
         this.xPath = xPath
         this.yPath = yPath
-        this.name = name
+        this.obj = obj
 
         planets.push(this)
     }
@@ -52,8 +54,61 @@ class planet {
         // A header for the html in every planet
         let text = document.createElement('h1')
         element.classList.add('planetText')
-        text.innerHTML = `Hello this is ${this.name}`
+        text.innerHTML = `Hello this is ${this.obj.name}`
         element.appendChild(text)
+
+        text = document.createElement('p');
+        element.classList.add('planetText')
+        text.innerHTML = `${this.obj.question}`;
+        element.appendChild(text)
+
+        let input = document.createElement('input');
+
+        element.appendChild(input)
+        text = document.createElement('p')
+        element.appendChild(text)
+
+        let submit = document.createElement('button');
+        submit.innerHTML = 'submit';
+
+        submit.addEventListener('click', () => {
+            if (input.value == this.obj.solution) {
+                
+                if (this.obj.name == "Sun") {
+                    root.removeChild(element);
+                    text.innerHTML =
+                    "<h1>You did it lets go</h1>" +
+                    "<p>How does it feel? Are you happy?</p>" +
+                    "<p>You have just wasted a few minutes of your life</p>" +
+                    "<p>And the answer was just 0, 0... 0... 0... 0... </p>" +
+                    "<p>Well your stuck now, with me and this text</p>" + 
+                    "<p>Well you could just close the tab, but thats</p>" +
+                    "<p>but thats kinda rude, so i hope you stay :) </p>" +
+                    "<p> o </p>" + "<p> o </p>" + "<p> o </p>" +
+                    "<p>Did you know that planets are round?</p>" +
+                    "<p>Kinda cool right, oh you already knew?</p>" +
+                    "<p>No your stupid thats impossiblebles</p>" +
+                    "<p>Okay bye now</p>";
+
+                    element = document.createElement('div');
+                    element.classList.add("planetText")
+                    element.appendChild(text);
+                    root.appendChild(element);
+                    answerDiv.style.display = "none";
+
+                    
+                } else {
+
+                    text = document.createElement('h1');
+                    text.innerHTML = "correct answer, now go back :)"
+                    element.appendChild(text)
+
+                    document.getElementById(`${this.obj.variableName}`).innerHTML = `${this.obj.variableName} = ${this.obj.solution}`
+                }
+
+            }
+        })
+        element.appendChild(submit)
 
         // A back button to return to the solar system
         let back = document.createElement('button')
@@ -63,7 +118,8 @@ class planet {
             root.removeChild(element)
             canvas.style.display = 'block'
             root.style.display = 'none'
-            runSpeed = originalSpeed 
+            runSpeed = originalSpeed
+
         } )
 
         element.appendChild(back)
@@ -110,51 +166,45 @@ function drawPlanet(obj) {
 
 }
 
-// A function that takes a array, the array will specifically
-// contain specific information based on the index before inputed
+// Objects for information in the the planets
+let arrOfPlanetInfo = []
 
-
-
-// planets objects
-let sun = new planet(50, 1, 0, 0, 'Sun')
-let mercury = new planet(12, 1, 125, 100, 'Mercury')
-let venus = new planet(18, 1.2, 185, 160, 'Venus')
-let earth = new planet(20, 1.5, 250, 225, 'Earth')
-let mars = new planet(14, 1.8, 300, 290, 'Mars')
-let jupiter = new planet(40, 2.5, 400, 400, 'Jupiter')
-let saturn = new planet(28, 3, 500, 480, 'Saturn')
-let uranus = new planet(20, 3.4, 620, 600, 'Uranus')
-let neptune = new planet(19, 4, 700, 690, 'Neptune')
-
-
-// The function that loops every aniation frame
-function animate(){
-
-    requestAnimationFrame(animate);
-    globalNum();
-
-
-    // Clears the canvas
-    c.beginPath();
-    c.fillStyle = '#000000'
-    c.fillRect(-canvas.width,-canvas.height, canvas.width*2, canvas.height*2)
-    c.closePath()
-
-    // Sets colors to white
-    c.strokeStyle = '#ffffff';
-    c.fillStyle = '#ffffff';
+function planetInfo(name, question, solution, variableName) {
     
-    // Loops through the planets array and draws them onto the canvas
-    for (let val in planets) {
-        drawPlanet(planets[val])
+    let obj = {};
 
-    }
+    obj.name = name;
+    obj.question = question;
+    obj.solution = solution;
+    obj.variableName = variableName
+
+    arrOfPlanetInfo.push(obj);
 
 
 }
 
-animate();
+planetInfo("Sun", "What is the soultion to:   m^2 * ( sqrt(v)/E^2 ) + j^s - d/dx( u*m ) * d/dx( n ) ", "0", "io");
+planetInfo("Mercury", "What is the soultion to: m = 9 + 10", "21", "m");
+planetInfo("Venus", "What is the soultion to: ?", "?", "v");
+planetInfo("Earth", "What is the soultion to: ?", "?", "E");
+planetInfo("Mars", "What is the soultion to: ?", "?", "M");
+planetInfo("Jupiter", "What is the soultion to: ?", "?", "j");
+planetInfo("Saturn", "What is the soultion to: ?", "?", "s");
+planetInfo("Uranus", "What is the soultion to: ?", "?", "u");
+planetInfo("Neptune", "What is the soultion to: ?", "?", "n");
 
+
+
+// planets objects
+let sun = new planet(40, 1, 0, 0, arrOfPlanetInfo[0])
+let mercury = new planet(10, 1, 100, 75, arrOfPlanetInfo[1])
+let venus = new planet(15, 1.2, 160, 145, arrOfPlanetInfo[2])
+let earth = new planet(17, 1.5, 225, 200, arrOfPlanetInfo[3])
+let mars = new planet(11, 1.8, 275, 265, arrOfPlanetInfo[4])
+let jupiter = new planet(32, 2.5, 350, 350, arrOfPlanetInfo[5])
+let saturn = new planet(22, 3, 440, 440, arrOfPlanetInfo[6])
+let uranus = new planet(17, 3.4, 500, 520, arrOfPlanetInfo[7])
+let neptune = new planet(16, 4, 600, 570, arrOfPlanetInfo[8])
 
 // Event listeren for click events
 canvas.addEventListener('click', (event) => {
@@ -184,3 +234,32 @@ canvas.addEventListener('click', (event) => {
     }
 
 })
+
+
+// The function that loops every aniation frame
+function animate(){
+
+    requestAnimationFrame(animate);
+    globalNum();
+
+
+    // Clears the canvas
+    c.beginPath();
+    c.fillStyle = '#000000'
+    c.fillRect(-canvas.width,-canvas.height, canvas.width*2, canvas.height*2)
+    c.closePath()
+
+    // Sets colors to white
+    c.strokeStyle = '#ffffff';
+    c.fillStyle = '#ffffff';
+    
+    // Loops through the planets array and draws them onto the canvas
+    for (let val in planets) {
+        drawPlanet(planets[val])
+
+    }
+
+
+}
+
+animate();
